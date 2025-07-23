f5cbfdbe43f4f265fbd5f0cfa0e02c431355bdcc
audreylamwk <audreylamwk@gmail.com>
2025-07-24 00:41:13 +0100

    chore: lab01 exercise 5

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,19 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node * tortoise = head;
+    node * hare = head;
+    // need to check that pointer is not null before using it
+    while(hare && hare->next && hare->next->next){
+        hare = hare->next->next;
+        if (tortoise) {
+            tortoise = tortoise->next;
+        }
+        // pointer point to the same address
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
+    // return out of while loop
     return 0;
 }
