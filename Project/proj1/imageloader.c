d5d1ce0483d74caa563222c0da8d91065d050c04
audreylamwk <audreylamwk@gmail.com>
2025-07-24 03:05:53 +0100

    chore: project 1 task A1

--- Project/proj1/imageloader.c
+++ Project/proj1/imageloader.c
@@ -26,16 +26,60 @@
 Image *readData(char *filename) 
 {
 	//YOUR CODE HERE
+	FILE *fp = fopen(filename, "r");
+	Image * image = malloc(sizeof(Image));
+	// read or skip type
+	char* type;
+	fscanf(fp, "%s", type);
+	int width;
+	int height;
+	fscanf(fp, "%d %d",&width, &height);
+	image->rows = height;
+	image->cols = width;
+	int colour;
+	fscanf(fp, "%d",&colour);
+	// a list Color*
+	// each Color* store color of single pixel
+	image->image = malloc(height * width * sizeof(Color *));
+	for (int i=0; i< height * width; i++) {
+		Color * cColor = malloc(sizeof(Color));
+		fscanf(fp, "%d %d %d", &cColor->R, &cColor->G, &cColor->B);
+		image->image[i] = cColor;
+	}
+	fclose(fp);
+	return image;
+
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
 	//YOUR CODE HERE
+	printf("P3\n");
+	int cols = image->cols;
+	int rows = image->rows;
+	printf("%d %d\n", cols, rows);
+	printf("255");
+	for (int i=0; i<rows; i++) {
+		printf("\n");
+		for (int j=0; j<cols; j++) {
+			Color *pixel = image->image[i * cols + j];
+			printf("%3d %3d %3d", pixel->R, pixel->G, pixel->B);
+			if (j != cols - 1) {
+				printf("   ");
+			}
+		}
+	}
+	printf("\n");
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
+	for (int i =0; i < image->rows * image->cols; i++) {
+		free(image->image[i]);
+	}
+	free(image->image);
+	free(image);
 	//YOUR CODE HERE
 }
