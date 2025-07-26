65440e2739820c5ca08c34ff2caa599f8f34ec09
audreylamwk <audreylamwk@gmail.com>
2025-07-26 20:59:02 +0100

    chore: lab02 exercise 2

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -6,5 +6,12 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    unsigned xor0 = (* reg >> 2 & 0x1) ^ (* reg & 0x1);
+    unsigned xor1 = xor0 ^ (* reg >> 3 & 0x1);
+    unsigned xor2 = xor1 ^ (* reg >> 5 & 0x1);
+    // shift 1 bit to the right
+    * reg >>= 1;
+    // set 15th bit as result of xors
+    * reg |= (xor2 << 15);
 }
 
